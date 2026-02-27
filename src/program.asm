f06cc5abc002f23bbfc82b95c3496c33d36cd793
Henstly-main <kuzmikeduard3@gmail.com>
2026-02-27 22:47:51 +0400

    prompt added

--- src/program.asm
+++ src/program.asm
@@ -1,23 +1,115 @@
 org 0x7c00
 
-mov ah, 0x0e
-mov al, -1
-int 0x10
-mov al, ' '
-int 0x10
-mov al, -1
-neg al
-int 0x10
-mov al, ' '
-int 0x10
-cbw
-mov dl, ah
-mov ah,0x0e
-mov al, dl
-int 0x10
+start:
+	xor ax,ax
+	xor cx,cx
+	xor bx,bx
+	xor dx,dx
+	
+	
+main:
+	mov si, command
+	mov bx, 0
+	call print_string
+	call input
+	jmp main
 
-hang:
-	jmp $
+
+input:
+.start: ; save registers
+	pusha 
+	pushf
+	mov si, buffer
+	xor cx,cx
+.enter: ; input
+	mov ah, 0
+	int 0x16
+	
+	cmp cx, 99 ; check buffer
+	jnb .enter
+	
+	cmp al, 0x0d ; Enter -> done
+	je .done_input
+	
+	cmp al, 0x08 ; Backspace
+	je .call_backspace
+	
+	inc cx
+	mov [si], al
+	inc si
+	
+	mov ah, 0x0e
+	int 0x10
+	jmp .enter
+
+.call_backspace:
+	call .backspace
+	jmp .enter
+
+.backspace:
+	cmp cx, 0
+	je .return
+	
+	dec cx      ; dec cx and buffer
+	mov byte [si], 0
+	dec si
+	
+	mov ah,0x0e ; video backspace
+	mov al, 0x08
+	int 0x10
+	mov al, ' '
+	int 0x10
+	mov al, 0x08
+	int 0x10
+	jmp .return
+	
+.return:
+	ret
+	
+.done_input:
+	mov byte [si], 0
+	
+	mov al, 0x0d
+	mov ah,0x0e
+	int 0x10
+	mov al, 0x0a
+	int 0x10
+	popf
+	popa
+	ret
+	
+
+print_string:
+.print_start: ; push registers, cmp bx for favorite print
+	pusha
+	pushf
+	cmp bx, 0
+	je .print_command
+
+.print_command: ; print prompt
+	lodsb
+	cmp al, 0
+	je .done_print
+	
+	mov ah, 0x0e
+	int 0x10
+	jmp .print_command
+	
+.done_print:
+	popf
+	popa
+	ret
+	
+
+
+buffer:
+	times 100 db 0
+
+flag:
+	times 3 db 0
+
+command:
+	db '\>',0
 
 times 510-($-$$) db 0
 
